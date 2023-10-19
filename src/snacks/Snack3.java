b3e09578b94c14cb087af20b8593dba514011f7c
MirkoGLostia <giggi.torre@mail.com>
2023-10-19 14:44:04 +0200

    terzo snack ciclo for

--- src/snacks/Snack3.java
+++ src/snacks/Snack3.java
@@ -6,5 +6,15 @@ public class Snack3 {
         Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
      */
     public static void main(String[] args) {
+        int[] listOfNumbers = {2, 5, 8, 61, 78, 95, 4};
+        int summOfNumbers = 0;
+
+        for (int i = 0; i < listOfNumbers.length; i++) {
+            if (i % 2 == 1){
+                summOfNumbers += listOfNumbers[i];
+            }
+        }
+
+        System.out.println(summOfNumbers);
     }
 }
