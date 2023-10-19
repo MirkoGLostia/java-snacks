187b880681911189858547dbdb342d629fb4bcdd
MirkoGLostia <giggi.torre@mail.com>
2023-10-19 16:25:47 +0200

    quinto snack ciclo do while e 2 condizioni

--- src/snacks/Snack5.java
+++ src/snacks/Snack5.java
@@ -1,5 +1,7 @@
 package snacks;
 
+import java.util.Scanner;
+
 public class Snack5 {
     /*
     text exe:
@@ -11,6 +13,35 @@ public class Snack5 {
         Es. “25" come stringa deve essere convertito in intero 25.
      */
     public static void main(String[] args) {
+        // inizializzazione scanner
+        Scanner userInput = new Scanner(System.in);
 
+        // variabili uttili
+        int userStringToNumber = 0;
+        int i = 0;
+        boolean negativeStringValue = false;
+
+        // chiediamo un numero allo user
+        System.out.println("Gimme a number");
+        String userString = userInput.nextLine();
+
+        if( userString.charAt(0) == '-' ){
+            negativeStringValue = true;
+            i = 1;
+        }
+
+        do {
+            userStringToNumber *= 10;
+            userStringToNumber += ( userString.charAt(i++) - '0' );
+        }while (i < userString.length());
+
+        if (negativeStringValue){
+            userStringToNumber = -userStringToNumber;
+        }
+
+        System.out.println(userStringToNumber);
+
+        // chiudiamo scanner
+        userInput.close();
     }
 }
