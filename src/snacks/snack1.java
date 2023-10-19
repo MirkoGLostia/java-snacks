88d4f5e3b10ee8e7cb2a3b405576e2b062cb21e2
MirkoGLostia <giggi.torre@mail.com>
2023-10-19 14:19:28 +0200

    first snack implementazione while ed uscita

--- src/snacks/snack1.java
+++ src/snacks/snack1.java
@@ -1,7 +1,33 @@
 package snacks;
 
-public class snack1 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class snack1 {
+    /*
+    text exe:
+        Continua a chiedere all’utente di inserire un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+        Se il numero è negativo termina.
+     */
+    public static void main(String[] args) {
+        // inizializziamo lo scanner
+        Scanner userImput = new Scanner(System.in);
+
+        // chiediamo allo user un numero
+        System.out.println("Gimme a number:");
+        double number = Double.parseDouble(userImput.nextLine());
+
+        // iniziamo a scansionare i numeri, in modo da richiedere all'infinito in caso di numero pari, e terminare con negativo
+        while (number % 2 == 0){
+            System.out.print("gimme another number, maybe this time will end... ");
+            number = Double.parseDouble(userImput.nextLine());
+            if (number % 2 == 1){
+                number++;
+            }
+        }
+
+        System.out.println("YOU'VE DONE IT, THE LOOP IS CLOSED");
+
+        // terminiamo lo scanner
+        userImput.close();
     }
 }
