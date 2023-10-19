22d7afc09e5e5bcf37b53c602675281c6fea69e1
MirkoGLostia <giggi.torre@mail.com>
2023-10-19 15:01:30 +0200

    quarto snack

---
+++ src/snacks/Snack4.java
@@ -0,0 +1,11 @@
+package snacks;
+
+public class Snack4 {
+    /*
+    text exe:
+        Data in input una stringa verificare se è palindroma
+     */
+    public static void main(String[] args) {
+
+    }
+}
