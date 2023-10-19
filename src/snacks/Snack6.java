73fbecc25160aa88b0fe681819befa1f7de2bfb6
MirkoGLostia <giggi.torre@mail.com>
2023-10-19 16:47:25 +0200

    sesto snack conversione ore minuti secondi

--- src/snacks/Snack6.java
+++ src/snacks/Snack6.java
@@ -1,11 +1,45 @@
 package snacks;
 
+import java.util.Scanner;
+
 public class Snack6 {
     /*
     text exe:
         Scrivere un programma che dati dei secondi li converta in ore, minuti, secondi e mostri a video la stringa generata (secondi → “hh:mm:ss”)
      */
     public static void main(String[] args) {
+        // inizializzazione scan
+        Scanner userInput = new Scanner(System.in);
 
+        // apriamo variabile
+        String orarioUserConvertito = "";
+
+        // richiesta secondi all'utente
+        System.out.println("Gimme the seconds");
+        int userSeconds = userInput.nextInt();
+
+        for (int i = 0; i < 3; i++) {
+            if (i == 0){
+                // conversione ore
+                int hours = userSeconds / 3600;
+                orarioUserConvertito += hours + "-";
+                userSeconds = userSeconds-(hours*3600);
+            } else if (i == 1) {
+                // conversione minuti
+                int minutes = userSeconds / 60;
+                orarioUserConvertito += minutes + "-";
+                userSeconds = userSeconds-(minutes*60);
+            }else {
+                // aggiunta minuti
+                orarioUserConvertito += userSeconds;
+            }
+        }
+
+        // risultato
+        System.out.println("This is the equivalent in h-m-s:");
+        System.out.println(orarioUserConvertito);
+
+        // chiusura scanner
+        userInput.close();
     }
 }
