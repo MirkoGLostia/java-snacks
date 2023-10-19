705db57be782d0e205a5a7b56122f866cc34ded5
MirkoGLostia <giggi.torre@mail.com>
2023-10-19 14:36:47 +0200

    terzo snack

---
+++ src/snacks/Snack3.java
@@ -0,0 +1,11 @@
+package snacks;
+
+public class Snack3 {
+    /*
+    text exe:
+        Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole mostrare a video una falsa lista di invitati con nome e cognome.
+     */
+    public static void main(String[] args) {
+
+    }
+}
