273e343b842ed0be42e4be1fe3c07e444ff86809
MirkoGLostia <giggi.torre@mail.com>
2023-10-19 16:26:58 +0200

    sesto snack

---
+++ src/snacks/Snack6.java
@@ -0,0 +1,11 @@
+package snacks;
+
+public class Snack6 {
+    /*
+    text exe:
+        Scrivere un programma che dati dei secondi li converta in ore, minuti, secondi e mostri a video la stringa generata (secondi → “hh:mm:ss”)
+     */
+    public static void main(String[] args) {
+
+    }
+}
