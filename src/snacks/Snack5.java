3a39f6ebbaf4761d2b1170e64d99da1d0a1d3d18
MirkoGLostia <giggi.torre@mail.com>
2023-10-19 15:44:15 +0200

    quinto snack

---
+++ src/snacks/Snack5.java
@@ -0,0 +1,16 @@
+package snacks;
+
+public class Snack5 {
+    /*
+    text exe:
+        Dato un numero sotto forma di stringa, convertirlo in intero senza utilizzare funzioni già pronte.
+        Possibile usare solo :
+        cicli
+        chartAt
+        if / switch
+        Es. “25" come stringa deve essere convertito in intero 25.
+     */
+    public static void main(String[] args) {
+
+    }
+}
