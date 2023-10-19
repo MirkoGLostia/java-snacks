82f3ab51fb027f2d480d889a61c8af3f8fb94fe9
MirkoGLostia <giggi.torre@mail.com>
2023-10-19 14:34:34 +0200

    secondo snack array e ciclo for

--- src/snacks/Snack2.java
+++ src/snacks/Snack2.java
@@ -1,5 +1,7 @@
 package snacks;
 
+import java.util.Random;
+
 public class Snack2 {
     /*
     text exe:
@@ -7,6 +9,19 @@ public class Snack2 {
      */
     public static void main(String[] args) {
         // array nomi e cognomi
-        String[] nomiInvitaiti
+        String[] nomiInvitaiti = {"Mario", "Giovanni", "Francesco", "Dario", "Marcello"};
+        String[] cognomiInvitati = {"Rossi", "De Pauperis", "Galli", "Pisu", "Correlli"};
+
+        // inizializzazione randomizzatore
+        Random randomNumber = new Random();
+
+        // variabili utili
+        int numeroNomi = 5;
+        int numeroCognomi = 5;
+        int numeroInvitati = 10;
+
+        for (int i = 0; i < numeroInvitati; i++) {
+            System.out.println(nomiInvitaiti[randomNumber.nextInt(0, numeroNomi)] + " " + cognomiInvitati[randomNumber.nextInt(0, numeroCognomi)]);
+        }
     }
 }
